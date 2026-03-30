8162f7d46a4e16f89cf8f3b70694d35d6ef0b0fc
a-wlj-ddns-ip-net <aaa@wlj.ddns-ip.net>
2026-03-30 13:48:18 +0800

    Fix copyright notice in NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Nan1t
+ * Copyright (C) 2020 Nan1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
